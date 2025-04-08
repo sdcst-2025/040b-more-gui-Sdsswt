8a6bbcac90ccb14cbef8cddb5b366f219869a155
Sdsswt <terkeurswillem@gmail.com>
2025-04-08 11:19:05 -0700

    update

--- task1.py
+++ task1.py
@@ -1,10 +1,34 @@
 import tkinter as tk
+from tkinter import ttk
+from tkinter import*
 
 window = tk.Tk()
 window.title("tk")
-window.geometry("600x200")
+window.geometry("500x150")
 
 entry1 = tk.Entry(window,text="Entry widgets can be typed in",)
 
+label1 = tk.Label(window,text="principle")
+label1.place(x=50,y=10)
 
+label1 = tk.Label(window,text="intrest rate")
+label1.place(x=200,y=10)
+
+label1 = tk.Label(window,text="years")
+label1.place(x=350,y=10)
+
+label1 = tk.Label(window,text="amount")
+label1.place(x=100,y=75)
+
+entry1 = tk.Entry(window,text="")
+entry1.place(x=50,y=30)
+
+entry1 = tk.Entry(window,text="")
+entry1.place(x=200,y=30)
+
+entry1 = tk.Entry(window,text="")
+entry1.place(x=150,y=75)
+
+combobox1 = ttk.Combobox(window,text="")
+combobox1.place(x=350,y=30)
 window.mainloop()
---
+++ task2.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import*
+
+window = tk.Tk()
+window.title("")
+window.geometry("600x600")
+
+
+label1 = tk.Label(window,text="POKEMON ADVENTURES")
+label1.place(x=250,y=1)
+
+
+
+
+window.mainloop()
