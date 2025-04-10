bbddbd0f1f10c7c61fec8fb723356624972ecfd9
Sdsswt <terkeurswillem@gmail.com>
2025-04-10 11:01:49 -0700

    done

--- task1.py
+++ task1.py
@@ -4,31 +4,31 @@ from tkinter import*
 
 window = tk.Tk()
 window.title("tk")
-window.geometry("500x150")
+window.geometry("470x100")
 
 entry1 = tk.Entry(window,text="Entry widgets can be typed in",)
 
-label1 = tk.Label(window,text="principle")
-label1.place(x=50,y=10)
+label1 = tk.Label(window,text="Principle")
+label1.place(x=15,y=10)
 
-label1 = tk.Label(window,text="intrest rate")
-label1.place(x=200,y=10)
+label1 = tk.Label(window,text="Intrest rate")
+label1.place(x=165,y=10)
 
-label1 = tk.Label(window,text="years")
-label1.place(x=350,y=10)
+label1 = tk.Label(window,text="Years")
+label1.place(x=315,y=10)
 
-label1 = tk.Label(window,text="amount")
-label1.place(x=100,y=75)
-
-entry1 = tk.Entry(window,text="")
-entry1.place(x=50,y=30)
+label1 = tk.Label(window,text="Amount")
+label1.place(x=115,y=75)
 
 entry1 = tk.Entry(window,text="")
-entry1.place(x=200,y=30)
+entry1.place(x=15,y=30)
 
 entry1 = tk.Entry(window,text="")
-entry1.place(x=150,y=75)
+entry1.place(x=165,y=30)
+
+entry1 = tk.Entry(window,text="")
+entry1.place(x=165,y=75)
 
 combobox1 = ttk.Combobox(window,text="")
-combobox1.place(x=350,y=30)
+combobox1.place(x=315,y=30)
 window.mainloop()
--- task2.py
+++ task2.py
@@ -4,13 +4,69 @@ from tkinter import*
 
 window = tk.Tk()
 window.title("")
-window.geometry("600x600")
+window.geometry("650x600")
 
+logo = PhotoImage(file="logo.png")
+photo3 = tk.Label(window, image=logo)
+photo3.place(x=250,y=500)
+
+minimapimage = PhotoImage(file="main.png")
+photo1 = tk.Label(window, image=minimapimage)
+photo1.place(x=1,y=25)
+
+pokemonimage = PhotoImage(file="minimap.png")
+photo2 = tk.Label(window, image=pokemonimage)
+photo2.place(x=515,y=75)
 
 label1 = tk.Label(window,text="POKEMON ADVENTURES")
 label1.place(x=250,y=1)
 
+label1 = tk.Label(window,text="MINI MAP")
+label1.place(x=525,y=50)
 
+button = tk.Button(window,text="N ",height = 1, width = 2)
+button.place(x=40,y=500)
 
+button = tk.Button(window,text="NE",height = 1, width = 2)
+button.place(x=65,y=500)
+
+button = tk.Button(window,text="E",height = 1, width = 2)
+button.place(x=65,y=525)
+
+button = tk.Button(window,text="SE",height = 1, width = 2)
+button.place(x=65,y=550)
+
+button = tk.Button(window,text="S ",height = 1, width = 2)
+button.place(x=40,y=550)
+
+button = tk.Button(window,text="SW",height = 1, width = 2)
+button.place(x=15,y=550)
+
+button = tk.Button(window,text="W ",height = 1, width = 2)
+button.place(x=15,y=525)
+
+button = tk.Button(window,text="NW",height = 1, width = 2)
+button.place(x=15,y=500)
+
+buttonMap = tk.Button(window,text="MAP",height = 2, width = 12)
+buttonMap.place(x=520,y=185)
+
+buttonMap = tk.Button(window,text="INVENTORY",height = 2, width = 12)
+buttonMap.place(x=520,y=225)
+
+buttonMap = tk.Button(window,text="POKEDEX",height = 2, width = 12)
+buttonMap.place(x=520,y=265)
+
+buttonMap = tk.Button(window,text="ROSTER",height = 2, width = 12)
+buttonMap.place(x=520,y=305)
+
+buttonMap = tk.Button(window,text="JOURNAL",height = 2, width = 12)
+buttonMap.place(x=520,y=345)
+
+buttonMap = tk.Button(window,text="HELP",height = 2, width = 12)
+buttonMap.place(x=520,y=385)
+
+buttonMap = tk.Button(window,text="SHOP",height = 2, width = 12)
+buttonMap.place(x=520,y=425)
 
 window.mainloop()
